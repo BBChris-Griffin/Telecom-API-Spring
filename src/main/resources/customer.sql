f186672797578833cee20f69afa929a7928e69f0
Chris Griffin <32560724+BBChris-Griffin@users.noreply.github.com>
2021-08-10 13:54:22 -0500

    new new new scripts

--- src/main/resources/customer.sql
+++ src/main/resources/customer.sql
@@ -4,15 +4,13 @@ create table user (
 	email VARCHAR(50) NOT NULL,
 	password VARCHAR(50) NOT NULL,
     plan_id INT, 
-	foreign key (plan_id) references plan (plan_id),
-    device_id INT,
-    foreign key (device_id) references device (device_id)
+	foreign key (plan_id) references plan (plan_id)
 );
-insert into user (customer_id, name, email, password, plan_id, device_id) values (1, 'Bria Hillock', 'bhillock0@51.la', 'rGFtlTEpOnu', 1, 1);
-insert into user (customer_id, name, email, password, plan_id, device_id) values (2, 'Ced Delouch', 'cdelouch1@dion.ne.jp', '52YVbYVl', 2, 2);
-insert into user (customer_id, name, email, password, plan_id, device_id) values (3, 'Morna Summerrell', 'msummerrell2@washington.edu', 'zaWnsjk', 3, 3);
-insert into user (customer_id, name, email, password, plan_id, device_id) values (4, 'Jone Lammas', 'jlammas3@cbsnews.com', 'PEjXES6', 3, 4);
-insert into user (customer_id, name, email, password, plan_id, device_id) values (5, 'Georgie Hartil', 'ghartil4@slideshare.net', 'q6eGFatjq', 1, 5);
+insert into user (customer_id, name, email, password, plan_id) values (1, 'Bria Hillock', 'bhillock0@51.la', 'rGFtlTEpOnu', 1);
+insert into user (customer_id, name, email, password, plan_id) values (2, 'Ced Delouch', 'cdelouch1@dion.ne.jp', '52YVbYVl', 2);
+insert into user (customer_id, name, email, password, plan_id) values (3, 'Morna Summerrell', 'msummerrell2@washington.edu', 'zaWnsjk', 3);
+insert into user (customer_id, name, email, password, plan_id) values (4, 'Jone Lammas', 'jlammas3@cbsnews.com', 'PEjXES6', 3);
+insert into user (customer_id, name, email, password, plan_id) values (5, 'Georgie Hartil', 'ghartil4@slideshare.net', 'q6eGFatjq', 1);
 
 drop table user;
 
--- src/main/resources/device.sql
+++ src/main/resources/device.sql
@@ -3,16 +3,20 @@ create table device (
 	model VARCHAR(50),
 	phone_number VARCHAR(50),
     plan_id INT,
-	foreign key (plan_id) references plan (plan_id)
+	foreign key (plan_id) references plan (plan_id),
+    customer_id INT,
+    foreign key (customer_id) references user (customer_id)
 );
-insert into device (device_id, model, phone_number, plan_id) values (1, 'ANDROID', '871-988-5659', 1);
-insert into device (device_id, model, phone_number, plan_id) values (2, 'IOS', '322-585-3935', 2);
-insert into device (device_id, model, phone_number, plan_id) values (3, 'IOS', '497-536-3501', 2);
-insert into device (device_id, model, phone_number, plan_id) values (4, 'ANDROID', '200-877-0684', 3);
-insert into device (device_id, model, phone_number, plan_id) values (5, 'IOS', '719-355-5256', 1);
-insert into device (device_id, model, phone_number, plan_id) values (6, 'ANDROID', '137-483-6121', 1);
-insert into device (device_id, model, phone_number, plan_id) values (7, 'IOS', '755-885-1322', 1);
-insert into device (device_id, model, phone_number, plan_id) values (8, 'IOS', '353-326-3948', 3);
-insert into device (device_id, model, phone_number, plan_id) values (9, 'ANDROID', '490-914-0652', 1);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (1, 'ANDROID', '871-988-5659', 1, 1);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (2, 'IOS', '322-585-3935', 2, 1);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (3, 'IOS', '497-536-3501', 2, 2);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (4, 'ANDROID', '200-877-0684', 3, 3);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (5, 'IOS', '719-355-5256', 1, 1);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (6, 'ANDROID', '137-483-6121', 1, 1);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (7, 'IOS', '755-885-1322', 1, 1);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (8, 'IOS', '353-326-3948', 3, 3);
+insert into device (device_id, model, phone_number, plan_id, customer_id) values (9, 'ANDROID', '490-914-0652', 1, 1);
 
 drop table device;
+
+select * from device;
